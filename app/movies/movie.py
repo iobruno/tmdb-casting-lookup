d76d7099e6c06327b105d128835fca7e2d3ab82f
iobruno <bruno.ariev@gmail.com>
2021-02-04 15:04:56 -0300

    incl. poster_path

--- app/movies/movie.py
+++ app/movies/movie.py
@@ -12,19 +12,20 @@ class MovieDetails:
     original_title: str
     overview: str
     original_lang: str
+    poster_img_path: str
     release_date: str
     genres: List[str]
     actor_casting: List[Casting]
     is_adult: bool
 
     def __init__(self, **kwargs):
-        """media_type is either 'movie' or 'tv' """
         self.tmdb_id = kwargs.get('id')
         self.imdb_id = kwargs.get('imdb_id')
         self.title = kwargs.get('title')
         self.original_title = kwargs.get('original_title')
         self.overview = kwargs.get('overview')
         self.original_lang = kwargs.get('original_language')
+        self.poster_img_path = kwargs.get('poster_path')
         self.release_date = kwargs.get('release_date')
         self.is_adult = kwargs.get('adult')
         self.genres: List[str] = list(map(lambda genre: genre.get('name'),

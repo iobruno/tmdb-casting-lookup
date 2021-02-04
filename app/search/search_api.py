bba1fdd5e89112bf0988eb23d8ded08ca6314505
iobruno <bruno.ariev@gmail.com>
2021-02-04 15:40:56 -0300

    incl. mechanism to return keyed results

--- app/search/search_api.py
+++ app/search/search_api.py
@@ -1,5 +1,7 @@
 import os
 import requests as r
+from itertools import groupby
+from operator import attrgetter
 from typing import Dict, List
 
 from app.search.search_results import SearchResult
@@ -11,13 +13,22 @@ class SearchApi:
         self.bearer_token = bearer_token
         self.search_endpoint = "https://api.themoviedb.org/3/search/multi"
 
-    def query(self, query_string: str, language: str = "pt-br") -> List[SearchResult]:
+    def query(self, query_string: str, language: str = "pt-br") -> Dict[str, List[SearchResult]]:
+        """
+        returns a Dictionary might contain up to 3 keys 'movie', 'person', 'tv'
+        each key containing the list of matches for their respective key
+        :param query_string:
+        :return:
+        """
         multi_search = r.get(self.search_endpoint,
                              params={'query': query_string, 'language': language},
                              headers={"Authorization": f"Bearer {self.bearer_token}"})
         raw_results: List[Dict[str, str]] = multi_search.json().get('results')
         search_results: List[SearchResult] = self._parse(search_results=raw_results)
-        return search_results
+        grp_search_results = {key: list(val)
+                              for key, val in groupby(search_results, attrgetter('media_type'))}
+
+        return grp_search_results
 
     def _parse(self,
                search_results: List[Dict[str, str]],

d18ff4c58391b588cccd01dcd108ec07d3734ab3
iobruno <bruno.ariev@gmail.com>
2021-02-05 15:14:32 -0300

    fixing docstring

--- app/search/search_results.py
+++ app/search/search_results.py
@@ -13,12 +13,17 @@ class SearchResult:
     is_adult: bool
 
     def __init__(self, **kwargs):
-        """media_type is either 'movie' or 'tv' """
+        """
+        - media_type is either 'movie' or 'tv'
+        - title/original_title apply only for movies
+        - name/original_name apply only for tv_shows
+        """
+
         self.id = kwargs.get('id')
         self.media_type = kwargs.get('media_type')
-        self.title = kwargs.get('title')
-        self.original_title = kwargs.get('original_title')
+        self.title = kwargs.get('title') or kwargs.get('name')
+        self.original_title = kwargs.get('original_title') or kwargs.get('original_name')
         self.overview = kwargs.get('overview')
         self.original_lang = kwargs.get('original_language')
         self.release_date = kwargs.get('release_date')
-        self.is_adult = kwargs.get('adult')
+        self.is_adult = kwargs.get('adult') or False

93e35f0baf9bdb54d6bd6eb770bb31f071189632
iobruno <bruno.ariev@gmail.com>
2021-02-18 12:25:03 -0300

    incl. gcs api for image_upload

---
+++ tmdb/image/gcs_api.py
@@ -0,0 +1,23 @@
+import io
+
+from google.cloud import storage
+from google.cloud.storage import Bucket
+from PIL.JpegImagePlugin import JpegImageFile
+
+from tmdb.utils.config import Configuration
+
+
+class GCSApi:
+
+    def __init__(self):
+        cfg = Configuration.load_config()
+        client = storage.Client()
+        self.bucket: Bucket = client.get_bucket(cfg.gcloud.bucket_name)
+
+    def image_upload(self, picture: JpegImageFile, blob_name: str):
+        pic_buffer = io.BytesIO()
+        picture.save(pic_buffer, format='JPEG', quality=100)
+        blob = self.bucket.blob(blob_name)
+        blob.upload_from_string(pic_buffer.getvalue(), content_type="image/jpeg")
+        return "gs://{bucket_name}/{blob_name}".format(bucket_name=self.bucket.name,
+                                                       blob_name=blob_name)
